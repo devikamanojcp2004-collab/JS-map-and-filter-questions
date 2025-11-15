859e2a3e5309bcb6be6b7205b02c8ed21002580b
DEVIKA MANOJ C P <devikamanojcp2004@gmail.com>
2025-11-15 19:03:07 +0530

    object question

---
+++ practicequestions.js
@@ -0,0 +1,19 @@
+//(1)Create an object `person` with properties `name`, `age`, and `city`, and print each property
+//METHOD 1
+// let person={
+//     "name":"devika",
+//     "age":21,
+//     "city":"mahe"
+// }
+// console.log(Object.values(person));
+
+//METHOD 2[USING FOR ..IN]
+// let person={
+//     "name":"devika",
+//     "age":21,
+//     "city":"mahe"
+// }
+// for(let value in person){
+//     console.log(person[value]);
+    
+// }
